1f9f0aca7176be2904ecefab3ae3800322b646a7
Supakorn Wongsawang <supakorn.wo@ku.th>
2019-06-04 00:19:10 +0700

    can add delete items

--- src/App.js
+++ src/App.js
@@ -1,26 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import TodoList from './TodoList'
 import './App.css';
+import TodoItems from './TodoItems';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      items: [],
+      currentItem: {
+        "name": '',
+        "isComplete": false
+      }
+    }
+  }
+
+  componentDidMount() {
+    this.getItems()
+  }
+
+  getItems = e => {
+    axios.get(`https://localhost:5001/api/Todo/`).then(res => {
+      console.log(res)
+      const items = res.data
+      this.setState({ items: items })
+    })
+  }
+
+  handleInput = e => {
+    const itemText = e.target.value
+    const currentItem = { name: itemText, isComplete: false }
+    this.setState({ currentItem })
+  }
+  
+  addItem = e => {
+    e.preventDefault();
+    const newItem = this.state.currentItem
+    if (newItem.name !== '') {
+      console.log(newItem)
+      axios.post('https://localhost:5001/api/Todo/', newItem).then(e => this.getItems())
+      this.setState({
+        currentItem: {
+          "name": '',
+          "isComplete": false
+        }
+      })
+    }
+  }
+
+  deleteItem = id => {
+    axios.delete('https://localhost:5001/api/Todo/' + id).then(this.getItems())
+  }
+
+  inputElement = React.createRef()
+  render() {
+    return (
+      <div className="App">
+        <TodoList
+          addItem={this.addItem}
+          inputElement={this.inputElement}
+          handleInput={this.handleInput}
+          currentItem={this.state.currentItem}
+        />
+        <TodoItems
+          entries={this.state.items}
+          deleteItem={this.deleteItem}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
